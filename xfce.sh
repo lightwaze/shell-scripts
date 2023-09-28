39d4feb230451252dc7027bad44ab6fe69ea38fa
lightwaze <99588832+lightwaze@users.noreply.github.com>
2023-09-28 19:47:32 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -11,6 +11,7 @@ ninja install
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo edit /etc/pacman.conf
 # paru installation
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
@@ -18,5 +19,5 @@ makepkg -i
 # qemu/kvm/virt-manager setup
 sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
 sudo sytemctl start libvirtd 
-sudo vim /etc/libvirt/libvirtd.conf
-sudo systemctl restart libvirtd
+echo sudo vim /etc/libvirt/libvirtd.conf then restart libvirtd
+
