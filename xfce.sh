8acaf31ce384800cc515c7d410f68a8a883198ff
lightwaze <99588832+lightwaze@users.noreply.github.com>
2023-09-28 19:13:59 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,15 +1,17 @@
-pacman -S virtualbox git neofetch uwufetch htop btop steam wine winetricks meson sassc firefox jre-openjdk nodejs npm pnpm vim 
+sudo pacman -S git neofetch uwufetch htop btop steam wine winetricks meson sassc firefox jre-openjdk nodejs npm pnpm vim 
 git clone https://github.com/shimmerproject/Greybird
 cd Greybird
 meson --prefix=/usr builddir
 cd builddir
 ninja
 ninja install
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -i
-neofetch
-vim /etc/pacman.conf
+sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
+sudo sytemctl start libvirtd 
+sudo vim /etc/libvirt/libvirtd.conf
+sudo systemctl restart libvirtd
