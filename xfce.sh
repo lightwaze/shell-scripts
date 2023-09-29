14046761abf05acef98079d18855ddbdb87ecfb2
lightwaze <99588832+lightwaze@users.noreply.github.com>
2023-09-29 19:21:50 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -12,6 +12,8 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo edit /etc/pacman.conf
+# librewolf installation
+sudo pacman -S librewolf
 # paru installation
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
