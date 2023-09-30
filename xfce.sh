62b6077d3ea66c378f2c903e67e5441ad4f12f14
lightwaze <99588832+lightwaze@users.noreply.github.com>
2023-09-30 12:11:55 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,5 +1,5 @@
 # some essential packages
-sudo pacman -S git neofetch uwufetch htop btop steam wine winetricks meson sassc firefox jre-openjdk nodejs npm pnpm vim qbittorrent ffmpeg gufw
+sudo pacman -S git neofetch uwufetch htop btop steam wine winetricks meson sassc firefox jre-openjdk nodejs npm pnpm vim qbittorrent ffmpeg gufw less
 # greybird theme installation
 git clone https://github.com/shimmerproject/Greybird
 cd Greybird
@@ -12,12 +12,12 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo edit /etc/pacman.conf
-# librewolf installation
-sudo pacman -S librewolf
 # paru installation
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -i
+# librewolf installation
+paru -S librewolf-bin atlauncher-bin
 # qemu/kvm/virt-manager setup
 sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
 sudo sytemctl start libvirtd 
